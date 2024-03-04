2585c919a03582cd4657f71f2c6a5d52413f9b13
kyh174 <kyh174@naver.com>
2024-03-04 16:01:54 +0900

    정적 구현 먼저

--- src/App.js
+++ src/App.js
@@ -1,8 +1,95 @@
 import './App.css';
 
+function TableCategory({ category }) {
+  return (
+    <tr>
+      <th colSpan='2'>
+        {category}
+      </th>
+    </tr>
+  );
+}
+
+function TableRow({ product }) {
+  const name = product.stocked ? product.name : 
+    <span style={{ color: 'red' }}>
+      {product.name}
+    </span>;
+  
+  return (
+    <tr>
+      <td>{name}</td>
+      <td>{product.price}</td>
+    </tr>
+  );
+}
+
+function TableBox({ products }) {
+  let lastCategory = null;
+  let rows = [];
+
+  products.forEach( product => {
+    if (product.category !== lastCategory) {
+      rows.push(
+        <TableCategory
+          category={product.category}
+          key={product.category} />
+      );
+    }
+    rows.push(
+      <TableRow
+        product={product}
+        key={product.name} />
+    );
+    lastCategory = product.category;
+  });
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      <tbody>{rows}</tbody>
+    </table>
+  )
+}
+
+function SearchBar() {
+  return (
+    <form>
+      <input type='text' placeholder='Search...' />
+      <label>
+        <input type='checkbox' />
+        {' '}
+        Only show products in stock
+      </label>
+    </form>
+  );
+}
+
+function Main({ products }) {
+  return (
+    <>
+      <SearchBar />
+      <TableBox products={products} />
+    </>
+  )
+}
+
+const PRODUCTS = [
+  {category: "Fruits", price: "$1", stocked: true, name: "Apple"},
+  {category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit"},
+  {category: "Fruits", price: "$2", stocked: false, name: "Passionfruit"},
+  {category: "Vegetables", price: "$2", stocked: true, name: "Spinach"},
+  {category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin"},
+  {category: "Vegetables", price: "$1", stocked: true, name: "Peas"}
+];
+
 export default function App() {
   return (
-    <div className="App">
-    </div>
+    <Main products={PRODUCTS} />
   );
 }

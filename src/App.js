ddcdd4bd364e6efd37f76278bb75d2e44c0e6c8c
kyh174 <kyh174@naver.com>
2024-03-23 02:54:53 +0900

    Effect cleanup function

--- src/App.js
+++ src/App.js
@@ -16,20 +16,44 @@ function VideoPlayer({ src, isPlaying }) {
   return <video ref={ref} src={src} loop playsInline />;
 }
 
-export default function App() {
-  const [isPlaying, setIsPlaying] = useState(false);
-  const [text, setText] = useState('');
+function createConnection() {
+  // A real implementation would actually connect to the server
+  return {
+    connect() {
+      console.log('✅ Connecting...');
+    },
+    disconnect() {
+      console.log('❌ Disconnected.');
+    }
+  };
+}
 
-  return (
-    <>
-      <input value={text} onChange={e => setText(e.target.value)} />
-      <button onClick={() => setIsPlaying(!isPlaying)}>
-        {isPlaying ? 'Pause' : 'Play'}
-      </button>
-      <VideoPlayer
-        isPlaying={isPlaying}
-        src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4"
-      />
-    </>
-  );
+
+
+export default function App() {
+  // const [isPlaying, setIsPlaying] = useState(false);
+  // const [text, setText] = useState('');
+
+  // return (
+  //   <>
+  //     <input value={text} onChange={e => setText(e.target.value)} />
+  //     <button onClick={() => setIsPlaying(!isPlaying)}>
+  //       {isPlaying ? 'Pause' : 'Play'}
+  //     </button>
+  //     <VideoPlayer
+  //       isPlaying={isPlaying}
+  //       src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4"
+  //     />
+  //   </>
+  // );
+
+  useEffect(() => {
+    const connection = createConnection();
+    connection.connect();
+    
+    return () => {
+      connection.disconnect();
+    }
+  }, []);
+  return <h1>Welcome to the chat!</h1>;
 }
